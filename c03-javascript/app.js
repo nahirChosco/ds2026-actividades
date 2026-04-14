b502374de52881d7a6709bfcdd87c7d3832355e1
nahirChosco <nahirchosco2@gmail.com>
2026-04-13 22:15:58 -0300

    ej1-variables-operaciones

---
+++ c03-javascript/app.js
@@ -0,0 +1,10 @@
+const nombre = 'Nahir';
+const edad = 23;
+const materia = 'Desarrollo de Software'
+const mensaje = `me llamo ${nombre}, tengo ${edad} anios y curso ${materia}.`;
+console.log(mensaje);
+let contador = 0;
+for (let i=0; i<3;i++){
+    contador++;
+}
+console.log(contador);
