c1cb2d6ae11a5e6f631352f351927a1dc3ebf17e
nahirChosco <nahirchosco2@gmail.com>
2026-04-30 19:07:50 -0300

    ejercicio 1:fetch basico tipado

---
+++ Ejercicios/c05-async-await-fetch/ej1-usuarios/app.js
@@ -0,0 +1,5 @@
+import { obtenerUsuarios } from './api.js';
+const usuarios = await obtenerUsuarios();
+usuarios.forEach(usuario => {
+    console.log(`usuario: ${usuario.name} email: ${usuario.email}`);
+});
