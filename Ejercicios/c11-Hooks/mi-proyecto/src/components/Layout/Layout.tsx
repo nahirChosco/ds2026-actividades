2d53f2244305e03637dc2673ebdbfcc8ee139645
nahirChosco <nahirchosco2@gmail.com>
2026-06-28 13:13:41 -0300

    Incorporar Hooks y useFetch a la librería

---
+++ Ejercicios/c11-Hooks/mi-proyecto/src/components/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import Header from './Header'
+import Footer from './Footer'
+import type { ReactNode } from 'react';
+import Box from '@mui/material/Box';
+
+interface LayoutProps{
+    children: ReactNode;   
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+      <Box component="main" sx={{ flexGrow: 1 }}>
+        {children}
+      </Box>
+      <Footer />
+    </Box>
+  );
+}
